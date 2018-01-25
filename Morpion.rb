ad3eea084dbeb0f7ef1dbf5d9880732ec7cba973
Thomas 2 <thomas2@MacBook-Pro-de-Thomas.local>
2018-01-25 10:43:58 +0100

    Boardcase 1

--- Morpion.rb
+++ Morpion.rb
@@ -1,5 +1,23 @@
-class BoardCase  
-  def initialize
+class BoardCase
+	attr_accessor :etat, :nom
+
+  def initialize(etat,nom)
+  	@etat = etat
+  	@nom = nom
   	
   end
+
+  def etat
+  	return @etat
+	end
+
+
+	def nom
+		return @nom
+
+	end
 end
+a1 = BoardCase.new("vide","a1")
+
+p a1.nom
+p a1.etat

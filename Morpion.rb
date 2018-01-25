0d5c45464ac798fea6026a072a8f020695f689ba
Thomas 2 <thomas2@MacBook-Pro-de-Thomas.local>
2018-01-25 14:08:21 +0100

    interface faite toto

--- Morpion.rb
+++ Morpion.rb
@@ -4,18 +4,19 @@ class BoardCase
   attr_reader :nom
   attr_accessor :etat
   def initialize(etat,nom)
-    @etat = etat
+    $etat = etat
     @nom = nom
-    @etat = "vide"
+    $etat = "_"
   end
   def etat
-    return @etat
+    return $etat
   end
-  def nom
+=begin def nom
     return @nom
   end
+=end  
   def info
-    return "la case #{@nom} est #{@etat}"
+    return "la case #{@nom} est #{$etat}"
   end
 end
 
@@ -24,36 +25,31 @@ class Board
 
   def initialize
     puts "création du plateau"
-    
-    a1 = BoardCase.new("vide","a1")
-    a2 = BoardCase.new("vide","a2")
-    a3 = BoardCase.new("vide","a3")
-    b1 = BoardCase.new("vide","b1")
-    b2 = BoardCase.new("vide","b2")
-    b3 = BoardCase.new("vide","b3")
-    c1 = BoardCase.new("vide","c1")
-    c2 = BoardCase.new("vide","c2")
-    c3 = BoardCase.new("vide","c3")
+
+    @a1 = BoardCase.new("_","a1")
+    @a2 = BoardCase.new("_","a2")
+    @a3 = BoardCase.new("_","a3")
+    @b1 = BoardCase.new("_","b1")
+    @b2 = BoardCase.new("_","b2")
+    @b3 = BoardCase.new("_","b3")
+    @c1 = BoardCase.new("_","c1")
+    @c2 = BoardCase.new("_","c2")
+    @c3 = BoardCase.new("_","c3")
 
     @boardcases = boardcases
-    @boardcases = [a1,a2,a3,b1,b2,b3,c1,c2,c3]
-    def info_plateau
-    return "le plateau est composé de #{@boardcases}"	
-    end
-    
+    @boardcases = [@a1,@a2,@a3,@b1,@b2,@b3,@c1,@c2,@c3]
   end
-
-
+  
+  def interface
+    puts 	"#{@a1.etat} | #{@a2.etat} | #{@a3.etat}"
+    puts	"#{@b1.etat} | #{@b2.etat} | #{@b3.etat}"
+    puts	"#{@c1.etat} | #{@c2.etat} | #{@c3.etat}"
+  end
 end
+#---------------------------
 
 plateau = Board.new
-pp plateau.info_plateau
-
-
-
-
-
-
+p plateau.interface
 
 
 
